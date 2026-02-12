06674ec34fc2fcb51b6b9a25cb464553d61e1ad1
Ritu Kumari <2503.ritu@gmail.com>
2026-02-12 20:50:52 +0530

    Django QR Code project initialised

---
+++ django_qr/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from .forms import QRCodeForm
+import qrcode
+
+def generate_qr_code(request):
+    if request.method == 'POST':
+        form = QRCodeForm(request.POST)
+        if form.is_valid():
+            res_name = form.cleaned_data['restraunt_name']
+            url = form.cleaned_data['url']
+
+            # Generate QR Code 
+            qr = qrcode.make(url)
+            file_name = res_name.replace(" ","_").lower()+'_menu.png'
+            qr.save(file_name)
+            return render(request, 'qr_result.html')
+    else:
+        form = QRCodeForm
+        context = {
+            'form': form
+        }
+        return render(request, 'generate_qr_code.html', context)
