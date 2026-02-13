39fda3511e161f11c39b4583edf0a34ac6706064
Ritu Kumari <2503.ritu@gmail.com>
2026-02-13 11:51:15 +0530

    Django-QR Code Project Finished

--- django_qr/views.py
+++ django_qr/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .forms import QRCodeForm
 import qrcode
+import os
+from django.conf import settings
 
 def generate_qr_code(request):
     if request.method == 'POST':
@@ -11,12 +13,23 @@ def generate_qr_code(request):
 
             # Generate QR Code 
             qr = qrcode.make(url)
-            file_name = res_name.replace(" ","_").lower()+'_menu.png'
-            qr.save(file_name)
-            return render(request, 'qr_result.html')
+            file_name = res_name.replace(" ", "_").lower() + '_menu.png'
+            file_path = os.path.join(settings.MEDIA_ROOT, file_name) 
+            qr.save(file_path)
+
+            # Create QR-Image URL 
+            qr_url = os.path.join(settings.MEDIA_URL, file_name)
+            
+
+            context = {
+                'res_name': res_name,
+                'qr_url': qr_url,
+                'file_name': file_name,
+            }
+            return render(request, 'qr_result.html', context)
     else:
         form = QRCodeForm
         context = {
-            'form': form
+            'form': form,
         }
         return render(request, 'generate_qr_code.html', context)
